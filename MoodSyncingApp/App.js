90501da05cfb004d0ce18419d4366379ddf7db0b
Sasmitha Uvindu Jayasinghe <158791024+jayasinghesasmitha@users.noreply.github.com>
2025-07-05 21:27:36 +0530

    Update App.js

--- MoodSyncingApp/App.js
+++ MoodSyncingApp/App.js
@@ -3,12 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useEffect } from 'react';
 import { LogBox } from 'react-native';
+
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import CameraScreen from './screens/CameraScreen';
+import VoiceRecord from './screens/VoiceRecord'; // Add this line
 
-// Ignore specific warnings
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
 ]);
@@ -35,9 +36,12 @@ export default function App() {
         <Stack.Screen 
           name="Camera" 
           component={CameraScreen}
-          options={{
-            gestureEnabled: false
-          }}
+          options={{ gestureEnabled: false }}
+        />
+        <Stack.Screen 
+          name="VoiceRecord" 
+          component={VoiceRecord}
+          options={{ gestureEnabled: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
